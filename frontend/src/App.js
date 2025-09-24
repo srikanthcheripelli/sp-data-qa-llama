b4352f28f16daa5a1583a5595fe6019e665528a0
srikanthcheripelli <harika@unknownd26033a70d4c.home>
2025-09-24 01:14:55 -0700

    Initial commit: LLaMA Q&A backend + frontend copy

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import QnA from "./components/QnA.js";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>SP Data Q&A</h1>
+      <QnA />
+    </div>
+  );
+}
+
+export default App;
+
